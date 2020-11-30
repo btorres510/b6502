40349890f6e225cbb551eb47a31fc8110f3c6821
Byron Torres <btorres3@hawk.iit.edu>
2020-11-30 00:57:38 -0600

    add reset manager struct to handle reset events

---
+++ include/b6502/reset_manager.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/// @file reset_manager.h
+
+#include "b6502/base.h"
+
+#define MAX_DEVICES 30
+
+/**
+ *  @brief A function variable that points to the reset function of a bus component
+ */
+typedef uint8_t (*reset_handler)(void*);
+
+typedef struct ResetManager {
+  size_t num_devices;
+  struct {
+    void* obj;
+    reset_handler reset;
+  } devices[MAX_DEVICES];
+} ResetManager;
+
+ResetManager* reset_manager_create(void);
+void add_device(ResetManager* rm, void* obj, reset_handler reset);
+void reset(ResetManager* rm);
---
+++ src/reset_manager.c
@@ -0,0 +1,35 @@
+#include "b6502/reset_manager.h"
+
+#include <assert.h>
+
+#include "b6502/rc.h"
+
+static void deinit(void* obj) {
+  ResetManager* rm = obj;
+  for (size_t i = 0; i < rm->num_devices; i++) {
+    if (LIKELY(rm->devices[i].obj != NULL)) {
+      rc_weak_release((void*)&rm->devices[i]);
+    }
+  }
+}
+
+ResetManager* reset_manager_create(void) {
+  ResetManager* rm = rc_alloc(sizeof(*rm), deinit);
+  return rm;
+}
+
+void add_device(ResetManager* rm, void* obj, reset_handler reset) {
+  assert(rm->num_devices < MAX_DEVICES);
+  rm->devices[rm->num_devices].obj = obj;
+  rm->devices[rm->num_devices++].reset = reset;
+}
+
+void reset(ResetManager* rm) {
+  for (size_t i = 0; i < rm->num_devices; i++) {
+    if (LIKELY(rm->devices[i].obj != NULL)) {
+      void* obj = rm->devices[rm->num_devices].obj;
+      reset_handler reset = rm->devices[rm->num_devices].reset;
+      (reset)(obj);
+    }
+  }
+}
