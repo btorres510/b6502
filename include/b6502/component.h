663503e5821d0d53464978348a7538c0e12c0b43
Byron Torres <btorres3@hawk.iit.edu>
2020-11-30 00:40:36 -0600

    remove reset and read only

--- include/b6502/component.h
+++ include/b6502/component.h
@@ -6,16 +6,6 @@
 
 #include "b6502/base.h"
 
-#define _read base.read
-#define _write base.write
-#define _reset base.reset
-#define _read_only base.read_only
-
-/**
- *  @brief A function variable that points to the reset function of a bus component
- */
-typedef void (*reset_handler)(void *);
-
 /**
  *  @brief A function variable that points to the read function of a bus component
  */
@@ -30,8 +20,6 @@ typedef void (*write_handler)(void *, uint16_t addr, uint8_t val);
  *  @brief A generic struct for devices on the communication bus.
  */
 typedef struct Component {
-  reset_handler reset;
   read_handler read;
   write_handler write;
-  bool read_only;
 } Component;
