ab967ee0cba6bce7f562a086e303db94efae9590
Byron Torres <btorres3@hawk.iit.edu>
2020-11-30 21:12:32 -0600

    skeleton for getopt_long in main

--- standalone/src/main.c
+++ standalone/src/main.c
@@ -8,38 +8,35 @@
 
 #include "b6502/base.h"
 
-static struct option long_options[]
-    = {{"interactive", no_argument, 0, 'i'}, {"rom", no_argument, 0, 'r'}, {0, 0, 0, 0}};
+static struct option long_options[] = {{"rom", required_argument, 0, 'r'},
+                                       {"system", required_argument, 0, 's'},
+                                       {"help", no_argument, 0, 'h'},
+                                       {0, 0, 0, 0}};
+
+static void print_help(void) {}
 
 int main(int argc, char **argv) {
   int c = 0;
   char *rom = NULL;
-  bool interactive = false;
+  char *sys = NULL;
 
-  opterr = 0;  // Disable getopt() error printing
-  while ((c = getopt_long(argc, argv, "sr:", long_options, NULL)) != -1) {
+  while ((c = getopt_long(argc, argv, "r:s:h", long_options, NULL)) != -1) {
     switch (c) {
       case 's':
-        interactive = true;
+        sys = optarg;
         break;
       case 'r':
         rom = optarg;
         break;
+      case 'h':
+        print_help();
+        break;
       case '?':
-        if (optopt == 'r' || strcmp(optarg, "rom") != 0) {
-          LOG_ERROR("Option -%c requires an argument.\n", optopt);
-        } else if (isprint(optopt)) {
-          LOG_ERROR("Unknown option `-%c'.\n", optopt);
-        } else {
-          LOG_ERROR("Unknown option character `\\x%x'.\n", optopt);
-        }
-
         return EXIT_FAILURE;
       default:
         abort();
     }
   }
 
-  printf("Hello world!");
   return EXIT_SUCCESS;
 }
